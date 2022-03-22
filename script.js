68128c9a805b8966e003c4be74cdefe51e1edb13
Kevin Li <Kevinstart14@gmail.com>
2022-03-22 17:02:01 -0300

    feito 7

--- script.js
+++ script.js
@@ -6,7 +6,22 @@ botao.addEventListener('click', realizarTarefa);
 function realizarTarefa() {
   let escrita = document.createElement('li');
   escrita.innerText = texto.value;
+  escrita.classList.add('word');
   let amazenar = document.querySelector('#lista-tarefas');
   amazenar.appendChild(escrita);
-  texto.value = ''
+  texto.value = '';
 }
+
+function mudaCor() {
+    document.addEventListener('click', function (event) {
+      if (
+        event.target.classList.contains('word')
+      ) {
+          event.target.style.backgroundColor = 'gray'
+      }
+    }
+    )
+}
+mudaCor();
+
+
